43a3cf11c6662882361072e841942ddcf9b4db61
yusalz <84872458+yusalz@users.noreply.github.com>
2021-12-21 03:10:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import http from 'http';
 import appSrc from './app.js';
 import {MongoClient} from 'mongodb';
 import { writeFileSync } from 'fs';
-const app = appSrc(express, bodyParser, createReadStream, crypto, http, MongoClient, writeFileSync);
+import cors from './cors.js';
+const app = appSrc(express, bodyParser, createReadStream, crypto, http, MongoClient, writeFileSync, cors);
 app.listen(process.env.PORT || 8080, function() {
     console.log('server running on port 3000', '');
 });
